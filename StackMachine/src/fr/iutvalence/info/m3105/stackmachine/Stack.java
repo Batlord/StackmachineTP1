674929fd4ffc4c4ca01f6e6c36ce8a9f11f25e40
metzlera <antonin.metzler@laposte.net>
2015-09-25 16:59:25 +0200

    I found it !

---
+++ StackMachine/src/fr/iutvalence/info/m3105/stackmachine/Stack.java
@@ -0,0 +1,16 @@
+package fr.iutvalence.info.m3105.stackmachine;
+
+public class Stack {
+
+	
+	public Stack(int i) throws InvalidParametersException{
+		
+		if (i < 0){
+			throw new InvalidParametersException();
+		}
+		
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
